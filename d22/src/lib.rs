9c08c8dbb99f76ae5f476120c06d0505d8c94d79
Jacob Henn <windowWeasel@outlook.com>
2022-07-29 15:30:44 -0700

    solve day 22

--- d22/src/lib.rs
+++ d22/src/lib.rs
@@ -1,7 +1,15 @@
 use std::{
     cmp::{max, min},
+    io,
     ops::RangeInclusive,
+    str::FromStr,
 };
+
+use anyhow::{bail, Context, Result};
+
+use derive_more::Display;
+
+use itertools::Itertools;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Dim {
@@ -10,18 +18,27 @@ pub enum Dim {
     Z,
 }
 
-#[derive(Clone, Debug, PartialEq, Eq)]
+#[derive(Clone, Debug, PartialEq, Eq, Display)]
+#[display(
+    fmt = "x={}..{},y={}..{},z={}..{}",
+    "self.x_range.start()",
+    "self.x_range.end()",
+    "self.y_range.start()",
+    "self.y_range.end()",
+    "self.z_range.start()",
+    "self.z_range.end()"
+)]
 pub struct Cuboid {
-    pub x_range: RangeInclusive<usize>,
-    pub y_range: RangeInclusive<usize>,
-    pub z_range: RangeInclusive<usize>,
+    pub x_range: RangeInclusive<i32>,
+    pub y_range: RangeInclusive<i32>,
+    pub z_range: RangeInclusive<i32>,
 }
 
 impl Cuboid {
     pub fn new(
-        x_range: RangeInclusive<usize>,
-        y_range: RangeInclusive<usize>,
-        z_range: RangeInclusive<usize>,
+        x_range: RangeInclusive<i32>,
+        y_range: RangeInclusive<i32>,
+        z_range: RangeInclusive<i32>,
     ) -> Self {
         Self {
             x_range,
@@ -30,7 +47,34 @@ impl Cuboid {
         }
     }
 
-    pub fn get(&self, dim: Dim) -> &RangeInclusive<usize> {
+    pub fn volume(&self) -> u64 {
+        (1 + self.x_range.end().abs_diff(*self.x_range.start()) as u64)
+            * (1 + self.y_range.end().abs_diff(*self.y_range.start()) as u64)
+            * (1 + self.z_range.end().abs_diff(*self.z_range.start()) as u64)
+    }
+
+    pub fn points(&self) -> impl Iterator<Item = (i32, i32, i32)> {
+        self.x_range
+            .clone()
+            .cartesian_product(self.y_range.clone())
+            .cartesian_product(self.z_range.clone())
+            .map(|((x, y), z)| (x, y, z))
+    }
+
+    pub fn contains(&self, (x, y, z): (i32, i32, i32)) -> bool {
+        self.x_range.contains(&x) && self.y_range.contains(&y) && self.z_range.contains(&z)
+    }
+
+    pub fn is_initial(&self) -> bool {
+        self.x_range.start() >= &-50
+            && self.x_range.end() <= &50
+            && self.y_range.start() >= &-50
+            && self.y_range.end() <= &50
+            && self.z_range.start() >= &-50
+            && self.z_range.end() <= &50
+    }
+
+    fn get(&self, dim: Dim) -> &RangeInclusive<i32> {
         match dim {
             Dim::X => &self.x_range,
             Dim::Y => &self.y_range,
@@ -38,7 +82,7 @@ impl Cuboid {
         }
     }
 
-    pub fn with_dim(self, dim: Dim, range: RangeInclusive<usize>) -> Self {
+    fn with_dim(self, dim: Dim, range: RangeInclusive<i32>) -> Self {
         match dim {
             Dim::X => Self {
                 x_range: range,
@@ -64,11 +108,11 @@ impl Cuboid {
             && self.z_range.end() >= other.z_range.start()
     }
 
-    pub fn partition_by(
+    fn partition_dim_into(
         &self,
-        dim: Dim,
-        subtrahend: RangeInclusive<usize>,
         cuboids: &mut Vec<Cuboid>,
+        dim: Dim,
+        subtrahend: &RangeInclusive<i32>,
     ) {
         if self.get(dim).start() < subtrahend.start() {
             cuboids.push(
@@ -84,7 +128,120 @@ impl Cuboid {
         ));
 
         if self.get(dim).end() > subtrahend.end() {
-            cuboids.push(self.clone().with_dim(dim, (*self.get(dim).end() + 1)..=*subtrahend.end()))
+            cuboids.push(
+                self.clone()
+                    .with_dim(dim, (*subtrahend.end() + 1)..=*self.get(dim).end()),
+            )
         }
+    }
+
+    /// partition `self` into smaller cuboids such that, when combined, they produce the subset of `self` which does not intersect with `other`.
+    pub fn partition_by(&self, other: Self) -> impl Iterator<Item = Cuboid> {
+        let mut tmp0 = Vec::with_capacity(6);
+        let mut tmp1 = Vec::with_capacity(6);
+        self.partition_dim_into(&mut tmp0, Dim::X, &other.x_range);
+        while let Some(cuboid) = tmp0.pop() {
+            if cuboid.intersects(&other) {
+                cuboid.partition_dim_into(&mut tmp1, Dim::Y, &other.y_range);
+            } else {
+                tmp1.push(cuboid);
+            }
+        }
+        while let Some(cuboid) = tmp1.pop() {
+            if cuboid.intersects(&other) {
+                cuboid.partition_dim_into(&mut tmp0, Dim::Z, &other.z_range);
+            } else {
+                tmp0.push(cuboid);
+            }
+        }
+
+        tmp0.into_iter().filter(move |c| !c.intersects(&other))
+    }
+}
+
+pub fn parse_range(s: &str) -> Result<RangeInclusive<i32>> {
+    let (start, end) = s.split_once("..").context("expected `..`")?;
+    Ok(start
+        .parse()
+        .with_context(|| format!("couldn't parse `{start}` as a i32"))?
+        ..=end
+            .parse()
+            .with_context(|| format!("couldn't parse `{end}` as a i32"))?)
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Display, Debug)]
+pub enum Polarity {
+    On,
+    Off,
+}
+
+impl FromStr for Polarity {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.trim() {
+            "on" => Ok(Self::On),
+            "off" => Ok(Self::Off),
+            s => bail!("{s} is not a valid polarity"),
+        }
+    }
+}
+
+pub fn get_input() -> impl Iterator<Item = Result<(Polarity, Cuboid)>> {
+    io::stdin().lines().map(|line| {
+        let line = line.context("couldn't read line from stdin")?;
+        let mut line = line.as_str();
+
+        let polarity_str;
+        (polarity_str, line) = line.split_once("x=").context("expected `x=`")?;
+        let x_range_str;
+        (x_range_str, line) = line.split_once(",y=").context("expected `,y=`")?;
+        let x_range = parse_range(x_range_str).context("couldn't parse x range")?;
+
+        let y_range_str;
+        (y_range_str, line) = line.split_once(",z=").context("expected `,z=`")?;
+        let y_range = parse_range(y_range_str).context("couldn't parse y range")?;
+
+        let z_range = parse_range(line).context("couldn't parse z range")?;
+
+        Ok((
+            polarity_str
+                .parse()
+                .context("couldn't parse polarity str")?,
+            Cuboid::new(x_range, y_range, z_range),
+        ))
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn partition3() {
+        let cuboid0 = Cuboid::new(1..=3, 0..=0, 2..=6);
+        let cuboid1 = Cuboid::new(3..=9, 0..=0, 0..=5);
+        assert!(cuboid0.intersects(&cuboid1));
+        assert!(cuboid1.intersects(&cuboid0));
+
+        let mut tmp0 = Vec::with_capacity(6);
+        let mut tmp1 = Vec::with_capacity(6);
+        cuboid0.partition_dim_into(&mut tmp0, Dim::X, &cuboid1.x_range);
+        while let Some(cuboid) = tmp0.pop() {
+            if cuboid.intersects(&cuboid1) {
+                cuboid.partition_dim_into(&mut tmp1, Dim::Y, &cuboid1.y_range);
+            } else {
+                tmp1.push(cuboid);
+            }
+        }
+        while let Some(cuboid) = tmp1.pop() {
+            if cuboid.intersects(&cuboid1) {
+                cuboid.partition_dim_into(&mut tmp0, Dim::Z, &cuboid1.z_range);
+            } else {
+                tmp0.push(cuboid);
+            }
+        }
+
+        println!("{tmp0:#?}");
     }
 }
--- d22/src/main.rs
+++ d22/src/main.rs
@@ -1,5 +1,64 @@
 use anyhow::{Context, Result};
 
+use d22::{Cuboid, Polarity};
+use log::trace;
+
 fn main() -> Result<()> {
+    simple_logger::init_with_env().context("couldn't initialize logger")?;
+    let input = d22::get_input();
+
+    let mut reactor: Vec<Cuboid> = Vec::new();
+    for item in input {
+        let (polarity, cuboid) = item.context("couldn't get input")?;
+
+        // uncomment this to solve part 1
+        // if !cuboid.is_initial() {
+        //     continue;
+        // }
+
+        if polarity == Polarity::On {
+            trace!("turning on {cuboid}");
+
+            let collisions: Vec<Cuboid> = reactor
+                .iter()
+                .filter(|c| c.intersects(&cuboid))
+                .cloned()
+                .collect();
+
+            let mut partitions = vec![cuboid];
+            while !partitions.is_empty() {
+                while let Some(partition) = partitions.pop() {
+                    if let Some(collision) = collisions.iter().find(|c| c.intersects(&partition)) {
+                        partitions.extend(partition.partition_by((*collision).clone()));
+                    } else {
+                        trace!("  turning on {partition}");
+                        reactor.push(partition);
+                    }
+                }
+            }
+        } else {
+            trace!("turning off {cuboid}");
+
+            // the indexes of cuboids in `reactor` which intersect with `cuboid`. this must be collected before it is iterated through so that `reactor` can be simultaneously modified.
+            let collisions: Vec<usize> = reactor
+                .iter()
+                .enumerate()
+                .rev()
+                .filter(|(_, c)| c.intersects(&cuboid))
+                .map(|(i, _)| i)
+                .collect();
+            for i in collisions {
+                // since `collisions` is ordered from high to low (notice the `rev` above), if the last element of `reactor` collides with `cuboid`, it will be the first one encountered in this loop, therefore removing the concern that `swap_remove` could change its position before it is reached.
+                let collision = reactor.swap_remove(i);
+                for partition in collision.partition_by(cuboid.clone()) {
+                    trace!("    replacing with {partition}");
+                    reactor.push(partition);
+                }
+            }
+        }
+    }
+
+    println!("{}", reactor.iter().map(Cuboid::volume).sum::<u64>());
+
     Ok(())
 }
